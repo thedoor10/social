694172d836801568594bbaad7f8d037cfad7c1c3
Serge <sjthedoor@yahoo.com>
2015-07-20 16:01:36 -0700

    Second commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'social'
-set :repo_url, 'https://github.com/yourname/social.git'
+set :repo_url, 'https://github.com/thedoor10/social.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-server 'your_instance_name',
+server 'ec2-52-8-49-218.us-west-1.compute.amazonaws.com',
   user: 'ubuntu', roles: %w{web app db}
 
 set :ssh_options, {
-  keys: 'your_key_file'
+    keys: '~/test1/serge-key-pair-uswestnca.pem.txt'
 }
