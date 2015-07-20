c0e738e60e949143321c332ab804f61835a96c34
Serge <sjthedoor@yahoo.com>
2015-07-20 16:15:51 -0700

    third commit3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ server 'ec2-52-8-49-218.us-west-1.compute.amazonaws.com',
   user: 'ubuntu', roles: %w{web app db}
 
 set :ssh_options, {
-    keys: '/../../serge-key-pair-uswestnca.pem.txt'
+    keys: '../../serge-key-pair-uswestnca.pem.txt'
 }
